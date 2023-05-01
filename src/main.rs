ae76935b928a8962bbc61a7e0a0ae8c92764b6b3
ZeroMarker <u78k4l67@163.com>
2023-05-01 12:57:23 +0000

    Add source code

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::io;
+// cargo new
+// cargo build
+// cargo run
+// cargo check
+fn main() {
+    let sum = add(3, 5);
+    println!("Hello, world!");
+    println!("The sum of a + b is {sum}");
+    guess();
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn guess() {
+    println!("Guess a number!");
+
+    println!("Please input your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed {guess}");
+}
